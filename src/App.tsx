c3d3641da92afb212eddf7fc0d7bf7607ae49527
Anna Kondrateva <85047120+AnnaKondrDeveloper@users.noreply.github.com>
2024-07-31 22:19:11 -0400

    feat: add git api in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import style from "./App.module.css"
+import axios from "axios"
+
+type UserType = {
+	login: string
+	id: number
+}
+
+type SearchUsersType = {
+	items: UserType[]
+}
 
 function App() {
 
-const [selectedUser, setSelectedUser] = useState <string| null> (null)
+const [selectedUser, setSelectedUser] = useState <UserType | null> (null)
+const [users, setUsers] = useState<SearchUsersType[]>([]);
+
+useEffect(() => {
+		if( selectedUser) {
+			document.title = selectedUser.login
+		}
+}, [selectedUser]
+)
+
+useEffect( () => {
+	axios.get("https://api.github.com/search/users?q=rasberry1")
+	.then ( res => {
+		setUsers(res.data.items)
+		console.log(users);
+	})
+})
 
 
   return (
@@ -13,14 +39,15 @@ const [selectedUser, setSelectedUser] = useState <string| null> (null)
 			<input className={style.input} placeholder='search' /> <button className={style.input_button}>FIND</button>
 		</div>
 		<ul className={style.list}>
-			{["Apple", "Orange"].map( user => 
+			{ users.map( user => 
 			<li 
 			className={selectedUser === user ? style.selected : ""}
-			key = {user}
-			onClick={() => {setSelectedUser(user)}
+			key = {user.id}
+			onClick={() => {
+				setSelectedUser(user);
+				}
 			}
-			> {user} </li>
-
+			> {user.login} </li>
 			)}
 		</ul>
     </div>
