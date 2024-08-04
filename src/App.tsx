e482fb90957e9a4b9d87b78df9b9051a83261dc7
Anna Kondrateva <85047120+AnnaKondrDeveloper@users.noreply.github.com>
2024-08-04 19:13:39 -0400

    feat: add info about user profile

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,30 @@ import { useEffect, useState } from "react"
 import style from "./App.module.css"
 import axios from "axios"
 
-type UserType = {
+type UserSearchType = {
 login: string
 id: number
 }
 
-// type SearchUsersType = {
-// 	items: UserType[]
-// }
+type SearchResult = {
+	items: UserSearchType[]
+}
+
+type UserType = {
+	login: string
+	avatar_url: string
+	location: string
+	followers: string
+}
 
 function App() {
 
-const [selectedUser, setSelectedUser] = useState <UserType | null> (null)
-const [users, setUsers] = useState<UserType[]>([]);
+const [selectedUser, setSelectedUser] = useState <UserSearchType | null> (null)
+const [users, setUsers] = useState<UserSearchType[]>([]);
 const [ tempSearch, setTempSearch] = useState("raspberry1")
 const [ searchTerm, setSearchTerm] = useState("raspberry1")
+const [ userDetails, setUserDetails] = useState <UserType | null> (null)
+
 
 useEffect(() => {
 	if( selectedUser) {
@@ -27,35 +36,58 @@ useEffect(() => {
 )
 
 useEffect( () => {
-axios.get(`https://api.github.com/search/users?q=${searchTerm}`)
+axios
+.get<SearchResult>(`https://api.github.com/search/users?q=${searchTerm}`)
 .then ( res => {
 	setUsers(res.data.items)
 })
 }, [searchTerm])
 
+useEffect( () => {
+	if( selectedUser) {
+		axios
+		.get<UserType>(`https://api.github.com/users/${selectedUser.login}`)
+		.then ( res => {
+			setUserDetails(res.data)
+			console.log(userDetails)
+		})
+	}
+	}, [selectedUser, userDetails])
 
 return (
 	<div className={style.main}>
 	<div>
-		<input className={style.input} 
-		placeholder='search' 
-		value={tempSearch}
-		onChange={ (e) => { setTempSearch( e.currentTarget.value) }}/> 
-		<button className={style.input_button}
-					onClick={() => setSearchTerm(tempSearch)}>FIND</button>
-	</div>
-	<ul className={style.list}>
-		{ users.map( user => 
-		<li 
-		className={selectedUser === user ? style.selected : ""}
-		key = {user.id}
-		onClick={() => {
-			setSelectedUser(user);
+		<div>
+			<input className={style.input} 
+			placeholder='search' 
+			value={tempSearch}
+			onChange={ (e) => { setTempSearch( e.currentTarget.value) }}/> 
+			<button className={style.input_button}
+						onClick={() => setSearchTerm(tempSearch)}
+						>FIND</button>
+		</div>
+		<ul className={style.list}>
+			{ users.map( user => 
+			<li 
+			className={selectedUser === user ? style.selected : ""}
+			key = {user.id}
+			onClick={() => {
+				setSelectedUser(user);
+				}
 			}
-		}
-		> {user.login} </li>
-		)}
-	</ul>
+			> {user.login} </li>
+			)}
+		</ul>
+	</div>
+
+	{ userDetails? 
+		<div className={style.profile}>
+		<img src={userDetails.avatar_url}></img>
+		<p>Location: {userDetails.location}</p>
+		<p>Followers: {userDetails.followers}</p>
+		</div>
+		: <div></div>
+}
 	</div>
 )
 }
